f89007d42530b7ad9bbb864af0906ca09a258f4e
DESKTOP-B5IRF34\shuoya <timehet556947@gmail.com>
2023-05-01 12:50:42 +0800

    done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let _a = 5;
+    if _a % 2 == 0 {
+        println!("Number {} is even", _a);
+    } else {
+        println!("Number {} is odd", _a);
+    }
 }
